6ba18d70a1c857a408af53a7b6e793d3a46d83e0
alessandrodiaz <alessdg02@gmail.com>
2023-10-12 21:52:59 -0500

    Font and theme fix

--- app/src/main/java/com/example/univallealtoque/presentation/sign_in/SignInScreen.kt
+++ app/src/main/java/com/example/univallealtoque/presentation/sign_in/SignInScreen.kt
@@ -1,15 +1,20 @@
 package com.example.univallealtoque.presentation.sign_in
 
 import android.widget.Toast
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -21,6 +26,10 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.material3.Typography
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import com.example.univallealtoque.R
 
 @Composable
@@ -44,28 +53,49 @@ fun SignInScreen(
     Column(
         modifier = Modifier
             .fillMaxWidth()
-        .fillMaxSize(),
+            .fillMaxSize()
+            .padding(8.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
-        ) {
+    ) {
 
         Text(
             text = stringResource(R.string.login_title),
-            fontSize = 24.sp
-            /*style = MaterialTheme.typography.displaySmall*/
+            style = MaterialTheme.typography.displayLarge,
+            color = Color.Black
         )
         Spacer(
-            modifier = modifier.size(30.dp)
+            modifier = modifier.size(40.dp)
         )
         Text(
             text = stringResource(R.string.login_description),
-            fontSize = 20.sp,
-            textAlign = TextAlign.Center
-            /*style = MaterialTheme.typography.displaySmall*/
+            textAlign = TextAlign.Center,
+            style = MaterialTheme.typography.displaySmall,
+            color = Color.Black
         )
         Spacer(
-            modifier = modifier.size(30.dp)
+            modifier = modifier.size(40.dp)
         )
+        /*Button(onClick = onSignInClick){
+            Row(
+                modifier = modifier
+                    .clickable(onClick = onSignInClick)
+                    .padding(8.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.google),
+                    contentDescription = stringResource(id = R.string.login_button),
+                    modifier = Modifier
+                        .size(24.dp)
+                        .padding(end = 8.dp)
+                )
+                Text(text = stringResource(id = R.string.login_button))
+            }
+        }*/
+
+
+
         Button(onClick = onSignInClick) {
             Text(text = "Iniciar sesión con Google")
         }
