2dc3e6664967ca6c7f6b10b2af547075ea7ee185
edinsonuwu <Vembrace@gmail.com>
2023-11-28 13:43:08 -0500

    hus completas

--- app/src/main/java/com/example/univallealtoque/MainActivity.kt
+++ app/src/main/java/com/example/univallealtoque/MainActivity.kt
@@ -1,23 +1,41 @@
  package com.example.univallealtoque
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.view.WindowInsetsControllerCompat
-import androidx.lifecycle.lifecycleScope
 import com.example.univallealtoque.data.DataStoreSingleton
-import com.example.univallealtoque.network.AlToqueServiceFactory
 import com.example.univallealtoque.theme.UnivalleAlToqueTheme
-import kotlinx.coroutines.launch
+import com.google.android.gms.tasks.OnFailureListener
+import com.google.android.gms.tasks.OnSuccessListener
+
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+
 
  class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         DataStoreSingleton.initialize(this)
+        val mAuth = FirebaseAuth.getInstance();
+        val user = mAuth.currentUser
 
-
+        fun signInAnonymously() {
+            mAuth.signInAnonymously().addOnSuccessListener(this, OnSuccessListener<AuthResult?> {
+                // do your stuff
+            })
+                .addOnFailureListener(this,
+                    OnFailureListener { exception -> Log.e(TAG, "signInAnonymously:FAILURE", exception) })
+        }
+        if (user != null) {
+            // do your stuff
+        } else {
+            signInAnonymously()
+        }
         WindowInsetsControllerCompat(window, window.decorView).isAppearanceLightNavigationBars = true
         setContent {
             UnivalleAlToqueTheme {
@@ -33,3 +51,4 @@ import kotlinx.coroutines.launch
  fun UnivalleAlToqueAppPreview() {
      UnivalleAlToqueApp()
  }
+
