75726a1346cf7402842929bf002dff1d49d1c1eb
edinsonuwu <edinson.dorado@correounivalle.edu.co>
2023-12-14 16:58:03 -0500

    creacion del request

--- app/src/main/java/com/example/univallealtoque/ui/CreateNewActivityScreen.kt
+++ app/src/main/java/com/example/univallealtoque/ui/CreateNewActivityScreen.kt
@@ -3,38 +3,26 @@ package com.example.univallealtoque.ui
 import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.KeyboardArrowDown
-import androidx.compose.material.icons.filled.MoreVert
-import androidx.compose.material3.AlertDialogDefaults.shape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -43,27 +31,44 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.univallealtoque.sign_in_express.LoginViewModelExpress
-import com.example.univallealtoque.sign_in_google.UserDataGoogle
-import java.util.Locale
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.focus.focusModifier
-import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.focus.onFocusEvent
-import androidx.compose.ui.layout.onPlaced
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.input.ImeAction
+import com.example.univallealtoque.model.UserDataResponseExpress
+
+data class CreateNewActivityRequest(
+    var nameOfActivity: String? = null,
+    var typeOfActivity: String? = null,
+    var description: String? = null,
+    var numberOfSpaceAvailable:Int? = null,
+    var schedule: Schedule
+)
+
+data class Schedule (
+    var mondayStart: String? = "00:00",
+    var mondayEnd: String? = "00:00",
+    var tuesdayStart: String? = "00:00",
+    var tuesdayEnd: String? = "00:00",
+    var wednesdayStart: String? = "00:00",
+    var wednesdayEnd: String? = "00:00",
+    var thursdayStart: String? = "00:00",
+    var thursdayEnd: String? = "00:00",
+    var fridayStart: String? = "00:00",
+    var fridayEnd: String? = "00:00",
+    var saturdayStart: String? = "00:00",
+    var saturdayEnd: String? = "00:00"
+)
+
 
 @OptIn(ExperimentalMaterial3Api::class, ExperimentalComposeUiApi::class)
 @Composable
@@ -72,9 +77,13 @@ fun CreateNewActivityScreen(
     navController: NavController,
     modifier: Modifier
 ) {
+
+
+
+
     val keyboardController = LocalSoftwareKeyboardController.current
-    var hasFocus by remember { mutableStateOf(false) }
-    var hasFocus1 by remember { mutableStateOf(false) }
+    var hasFocusNameOfActivity by remember { mutableStateOf(false) }
+    var hasFocusDescription by remember { mutableStateOf(false) }
     var myColor by remember { mutableStateOf(Color.Blue) }
     var text by remember { mutableStateOf("") }
     var textd by remember { mutableStateOf("") }
@@ -116,14 +125,14 @@ fun CreateNewActivityScreen(
                     .fillMaxWidth()
                     .border(
                         width = 2.dp,
-                        color = if (hasFocus) Color.Red else Color.Black,
+                        color = if (hasFocusNameOfActivity) Color.Red else Color.Black,
                         shape = RoundedCornerShape(12.dp)
                     )
                     .height(40.dp)
                     .padding(start = 16.dp, top = 8.dp, end = 8.dp)
                     .background(Color.White)
                     .onFocusEvent { focusState ->
-                        hasFocus = focusState.isFocused
+                        hasFocusNameOfActivity = focusState.isFocused
                     }
             )
 
@@ -155,14 +164,14 @@ fun CreateNewActivityScreen(
                     .fillMaxWidth()
                     .border(
                         width = 2.dp,
-                        color = if (hasFocus) Color.Red else Color.Black,
+                        color = if (hasFocusDescription) Color.Red else Color.Black,
                         shape = RoundedCornerShape(12.dp)
                     )
                     .height(120.dp)
                     .padding(start = 16.dp, top = 8.dp, end = 8.dp)
                     .background(Color.White)
                     .onFocusEvent { focusState ->
-                        hasFocus = focusState.isFocused
+                        hasFocusDescription = focusState.isFocused
                     }
             )
 
@@ -175,7 +184,7 @@ fun CreateNewActivityScreen(
                     .padding(top = 16.dp, bottom = 16.dp)
             )
 
-            DemoSearchableDropdown(myHint = "10", myArrayOptions = numberVacancies, optionalFieldHorario=null, myComponentWithDP = 300)
+            DemoSearchableDropdown(myHint = "Cupos", myArrayOptions = numberVacancies, optionalFieldHorario=null, myComponentWithDP = 300)
 
             Text(
                 text = "Horario:",
